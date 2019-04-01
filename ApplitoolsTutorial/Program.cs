96af8b52d113df9239d9a66e940c06a85316c634
Raja Rao DV <rajaraodv100@gmail.com>
2019-04-01 15:55:51 -0700

    first commit

---
+++ ApplitoolsTutorial/Program.cs
@@ -0,0 +1,85 @@
+﻿using Applitools;
+using Applitools.Selenium;
+using Applitools.VisualGrid;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Configuration = Applitools.Selenium.Configuration;
+
+
+namespace ApplitoolsTutorial
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            // Create a new webdriver
+            IWebDriver webDriver = new ChromeDriver();
+
+            // Navigate to the url we want to test
+            webDriver.Url = "https:/demo.applitools.com";
+
+            // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+            //but then change the above URL to https://demo.applitools.com/index_v2.html (for the 2nd run)
+
+            // Create a runner with concurrency of 10
+            VisualGridRunner runner = new VisualGridRunner(10);
+
+            // Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
+            Eyes eyes = new Eyes(runner);
+
+            //Set the Applitools API KEY here or as an environment variable "APPLITOOLS_API_KEY"
+            eyes.ApiKey = "APPLITOOLS_API_KEY";
+
+
+            // Create configuration object
+            Configuration conf = new Configuration();
+            
+
+            // Set test name
+            conf.TestName = "C# VisualGrid demo";
+
+            // Set app name
+            conf.AppName = "Demo app";
+
+            // Add browsers with different viewports
+            conf.AddBrowser(800, 600, Configuration.BrowserType.CHROME);
+            conf.AddBrowser(700, 500, Configuration.BrowserType.CHROME);
+            conf.AddBrowser(1200, 800, Configuration.BrowserType.FIREFOX);
+            conf.AddBrowser(1600, 1200, Configuration.BrowserType.FIREFOX);
+
+            // Add iPhone 4 device emulation in Portraig mode
+            EmulationInfo iphone4 = new EmulationInfo(EmulationInfo.DeviceNameEnum.iPhone_4, Applitools.VisualGrid.ScreenOrientation.Portrait);
+            conf.AddDeviceEmulation(iphone4);
+
+
+
+            // Set the configuration object to eyes
+            eyes.Configuration = conf;
+
+            // Call Open on eyes to initialize a test session
+            eyes.Open(webDriver);
+
+            // check the login page
+            eyes.Check(Target.Window().Fully().WithName("Login page"));
+            webDriver.FindElement(By.Id("log-in")).Click();
+
+            // Check the app page
+            eyes.Check(Target.Window().Fully().WithName("App page"));
+
+            // Close the browser
+            webDriver.Quit();
+
+            //Wait and collect all test results
+            TestResultSummary allTestResults = runner.GetAllTestResults();
+            System.Console.WriteLine(allTestResults);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Pre-requisites:
+
+1. Visual Studio installed on your machine.
+   * [Install it from here](https://visualstudio.microsoft.com/downloads/)
+2. Chrome Webdriver is on your machine and is in the PATH. Here are some resources from the internet that'll help you.
+   * https://splinter.readthedocs.io/en/0.1/setup-chrome.html
+   * https://stackoverflow.com/questions/38081021/using-selenium-on-mac-chrome
+   * https://www.youtube.com/watch?time_continue=182&v=dz59GsdvUF8
+
+# Steps to run this example
+
+1. Git clone this repo
+2. Open the folder
+3. Double click the `ApplitoolsTutorial.sln`. This will open the project in Visual Studio
+3. Change the APPLITOOLS_API_KEY with your own.
+  * Login to Applitools > Click on the Person icon > My API Key
+4. Hit Run
