a57c7158f7622548e54e4425f0c7173e3d7b9768
Pandy Knight <andylpk.247@gmail.com>
2022-12-28 20:19:30 -0500

    Created test project structure

---
+++ Applitools.Example.Tests/AcmeBankTest.cs
@@ -0,0 +1,15 @@
+namespace Applitools.Example.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
